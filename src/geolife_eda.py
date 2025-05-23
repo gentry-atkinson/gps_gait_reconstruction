3d30c807429e742884485754e56ef5570097685d
Gentry Atkinson <gatkinso@stedwards.edu>
2025-05-23 11:41:30 -0500

    getting requirements file started

---
+++ src/geolife_eda.py
@@ -0,0 +1,7 @@
+# Date: 23 May, 2025
+# Data loading functions along with some visualization and exploration thrown in.
+
+import pandas as pd
+
+def load_file_as_df(user: int, file_num: str) -> pd.DataFrame:
+    pass
