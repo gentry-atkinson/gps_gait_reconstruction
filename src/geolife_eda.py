62ed526c15e38a90d1f3aed74dd584edaea126b6
Gentry Atkinson <gatkinso@stedwards.edu>
2025-05-24 12:33:36 -0500

    load full geolife user as df

--- src/geolife_eda.py
+++ src/geolife_eda.py
@@ -10,11 +10,17 @@ def load_file_as_df(user: str, file_num: str) -> pd.DataFrame:
         # Skip top six lines
         f.readline(); f.readline(); f.readline(); f.readline(); f.readline(); f.readline()
         file_object = io.StringIO(f.read())
-        df = pd.read_csv(file_object)
+        df = pd.read_csv(file_object, index_col=False)
         df.columns = ['Latitude', 'Longitude', 'Set to 0', 'Altitude', 
                       'Days Since 12/30/1899', 'Date', 'Time']
         df.drop('Set to 0', axis=1, inplace=True)
         return df
+    
+def load_user_as_df(user: str) -> pd.DataFrame:
+    file_list = os.listdir(os.path.join('src', 'data', 'Geolife Trajectories 1.3', 'Data', user, 'Trajectory'))
+    return pd.concat([load_file_as_df(user, file.split('.')[0]) for file in file_list])
 
 if __name__ == '__main__':
-    print(load_file_as_df('000', '20081023025304').head())
+    df = load_user_as_df('000')
+    print(len(df))
+    print(df.tail())
